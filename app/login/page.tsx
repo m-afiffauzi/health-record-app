dae5564105da9ee1641a534dc9b1c1f007cb558b
Muhammad Afif Fauzi <afiffauzi234@gmail.com>
2023-08-29 14:07:00 +0700

    remove login default redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,6 +20,7 @@ export default function Login() {
     signIn("credentials", {
       email,
       password,
+      redirect: false,
     }).then((res) => {
       if (res?.error) {
         toast.error(res.error);
